5712fed0f2f84c10cda45ad34ffae4f39f07c296
Mirla Chucre <mirlabraga@gmail.com>
2021-03-23 09:32:59 +0000

    add backend tests

--- backend/tests/App.test.ts
+++ backend/tests/App.test.ts
@@ -1,10 +1,29 @@
+import { Document } from 'mongoose';
 import request from 'supertest';
 import { app } from "../src/App";
+import { Plans } from "../src/Plans.model";
 
-describe("GET / - the api endpoint for home page. When the use open the applicaton in the root page.", () => {
+describe("GET /", () => {
   it("The home page for subscription plans backend", async () => {
     const result = await request(app).get("/");
     expect(result.text).toEqual("Subscription plans backend!");
+    expect(result.status).toEqual(200);
+  });
+});
+
+describe("GET /plans", () => {
+  it("Should result in resolved list", async () => {
+    const plans = [new Plans({
+      code: 'gb',
+      name: 'UK',
+      monthlyCost: 10,
+      annualCost: 50,
+    })]
+    // jest.spyOn(Plans, 'find').res
+    jest.spyOn(Plans, 'find').mockResolvedValue(plans);
+
+    const result = await request(app).get("/plans");
+    expect(result.text).toEqual(JSON.stringify(plans));
     expect(result.status).toEqual(200);
   });
 });
